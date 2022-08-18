2f74f2064662fd0ebb557f6b9656a744bf38aa41
Dostmurod <dostmurodzoirov777@gmail.com>
2022-08-18 10:47:11 +0500

    js matematik amallar

---
+++ app.js
@@ -0,0 +1,251 @@
+// 10-misol
+
+// let ten = 86;
+// let numbers = 0;
+
+// for(let i = 1; i<= ten; i++){
+    
+//     let n = 1 / i;
+//     numbers = numbers + n
+// }
+
+// console.log(numbers);
+
+// swich 1-misol
+
+
+// let day = '2'
+
+// switch (day) {
+//     case '1':
+//         console.log('Dushanba')
+//         break;
+//     case '2':
+//         console.log('Seshanba');
+//         break;
+//     case '3':
+//         console.log('Chorshanba');
+//         break;
+//     case '4':
+//         console.log('Payshanba');
+//         break;
+//     case '5':
+//         console.log('Juma');
+//         break;
+//     case '6':
+//         console.log('SHanba');
+//         break;
+//     case '7':
+//         console.log('Yakshanba');
+//         break;
+//     default:
+//         alert('Bunaqa kun yoq')
+// }
+
+// 2-misol
+
+// let ball = '2'
+
+// switch (ball) {
+//     case '1':
+//         console.log('Yomon')
+//         break;
+//     case '2':
+//         console.log('Qonqarsiz');
+//         break;
+//     case '3':
+//         console.log('Qoniqarli');
+//         break;
+//     case '4':
+//         console.log('Yaxshi');
+//         break;
+//     case '5':
+//         console.log('Alo');
+//         break;
+//     default:
+//         alert('Bunaqa balni Topa olmadim')
+// }
+
+// 3-misol
+// let oy = '2'
+
+// switch (oy) {
+//     case '1':
+//         console.log('Qish')
+//         break;
+//     case '2':
+//         console.log('Bahor');
+//         break;
+//     case '3':
+//         console.log('Yoz');
+//         break;
+//     case '4':
+//         console.log('Kuz');
+//         break;
+//     default:
+//         alert('Bunaqa Faslni Topa olmadim')
+// }
+
+// 4-misol
+
+// let kun = '2'
+
+// switch (kun) {
+//     case '1':
+//         console.log(31 + " kun bor")
+//         break;
+//     case '2':
+//         console.log(30 + " kun bor");
+//         break;
+//     case '3':
+//         console.log(28 + " kun bor");
+//         break;
+//     case '4':
+//         console.log(30 + " kun bor");
+//         break;
+//     case '5':
+//         console.log(31 + " kun bor");
+//         break;
+//     case '6':
+//         console.log(30 + " kun bor");
+//         break;
+//     case '7':
+//         console.log(31 + " kun bor");
+//         break;
+//     case '8':
+//         console.log(30 + " kun bor");
+//         break;
+//     case '9':
+//         console.log(31 + " kun bor");
+//         break;
+//     case '10':
+//         console.log(30 + " kun bor");
+//         break;
+//     case '11':
+//         console.log(31 + " kun bor");
+//         break;
+//     case '12':
+//         console.log(30 + " kun bor");
+//         break;
+//     default:
+//         alert('Bunaqa kunni topa olmadim')
+// }
+
+// 5-misol
+// let c = +prompt('amalni tanlang');
+
+// let a = +prompt("sonni kiriting");
+// let b = +prompt(" sonni kiriting");
+
+// switch (c) {
+//     case "-":
+//         console.log(a - b);
+//         break;
+//     case "+":
+//         console.log(a + b);
+//         break;
+
+//     case "*":
+//         console.log(a * b);
+//         break;
+//     case "/":
+//         console.log(a / b);
+//         break
+// }
+
+//6-misol
+
+
+// let ball = '2'
+
+// switch (ball) {
+//     case '1':
+//         console.log('Yaxshilab oqing siznig stependiyangiz 50 ming')
+//         break;
+//     case '2':
+//         console.log('Yaxshi oqing siznig stependiyangiz 100 ming');
+//         break;
+//     case '3':
+//         console.log('Yaxshilab oqing siznig stependiyangiz 250 ming');
+//         break;
+//     case '4':
+//         console.log('Yaxshilab oqing siznig stependiyangiz 450 ming');
+//         break;
+//     case '5':
+//         console.log('Yaxshilab oqing siznig stependiyangiz 670 ming');
+//         break;
+//     default:
+//         alert('Bunaqa bahoni Topa olmadim')
+// }
+
+// 7-misol
+
+// let number = 200
+// for(let i = 0; i <= number -1; i++){
+//     console.log(number);
+// }
+
+// 8-misol
+// let a = 10;
+// let b = 5;
+
+// // let length = a - b;
+
+// for(let i = a; i >= b; i--){
+//     console.log(i);
+// }
+
+// 9-misol
+
+// let a = 10;
+// let b = 5;
+
+// for(let i = a -1; i >= b + 1; i--){
+//     console.log(i);
+// }
+ 
+// 10-misol
+
+// let kilo = 10;
+
+// for(let i = 1; i <= kilo; i++){
+//     console.log(`${i * kilo} ${i * 10000} so'm`);
+// }
+
+// 11-misol
+// let sum = 0;
+// let a = 2;
+// let b = 10;
+
+// for(let i = a; i <= b; i++ ){
+//     sum += i ;
+// } 
+
+// console.log(sum);
+
+// 12-misol
+
+// let sum = 1;
+// let a = 2;
+// let b = 10;
+
+// for(let i = a; i <= b; i++ ){
+//     sum *= i ;
+// } 
+
+// console.log(sum);
+
+// 13-misol
+
+// let n = 10;
+// let sum = 0;
+// let factorial = 1;
+
+// for(let i = 1; i <= n; i++){
+//     factorial *= i ;
+    
+// }
+
+
+
+
